1804113f71c8c6bafb9877de02663b6403769d59
matheus simoes <matheusaugustosimoes@hotmail.com>
2020-10-08 00:20:12 -0300

    Updates

---
+++ precache-manifest.b4250c3e7dc43d2bd7baaadbbdee51c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594a1c949f8e102a519e6d821f7c3758",
+    "url": "/React-Pokedex/index.html"
+  },
+  {
+    "revision": "4944d6fa5c76c00d9c95",
+    "url": "/React-Pokedex/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "536cabd46c68c7d46600",
+    "url": "/React-Pokedex/static/css/main.3c5ee70b.chunk.css"
+  },
+  {
+    "revision": "4944d6fa5c76c00d9c95",
+    "url": "/React-Pokedex/static/js/2.1b97b158.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Pokedex/static/js/2.1b97b158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536cabd46c68c7d46600",
+    "url": "/React-Pokedex/static/js/main.24bc0e05.chunk.js"
+  },
+  {
+    "revision": "17375ea93308199b1eba",
+    "url": "/React-Pokedex/static/js/runtime-main.fd66bc9c.js"
+  }
+]);
